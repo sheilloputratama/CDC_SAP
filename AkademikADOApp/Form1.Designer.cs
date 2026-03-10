5db28dfe4c0d2c19a9447c6294d614042377101f
sheilloputratama <sheilloggendut@gmail.com>
2026-03-10 12:33:04 +0700

    Menambahkan button 1

--- AkademikADOApp/Form1.Designer.cs
+++ AkademikADOApp/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(252, 302);
+            this.button1.Location = new System.Drawing.Point(277, 313);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;

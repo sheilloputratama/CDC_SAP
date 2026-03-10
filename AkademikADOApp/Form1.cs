fbc174bb60e5aeaf6d265e8eacd4ccdc07bfbf9d
sheilloputratama <sheilloggendut@gmail.com>
2026-03-10 12:51:07 +0700

    menghubungkan dengan sql server

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -4,11 +4,15 @@ using System.Windows.Forms;
 
 namespace AkademikADOApp
 {
+ 
     public partial class Form1 : Form
     {
+        string connString =
+       "Data Source=LAPTOP-54NIAI63\\SHEILLOAGRA;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
         }
+
     }
 }

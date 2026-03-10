d30879bf2edfbb0441b581eaae5ceb3761837197
sheilloputratama <sheilloggendut@gmail.com>
2026-03-10 12:35:19 +0700

    mengganti nama button 1 menjadi Connect Database

--- AkademikADOApp/Form1.Designer.cs
+++ AkademikADOApp/Form1.Designer.cs
@@ -34,11 +34,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(277, 313);
+            this.button1.Location = new System.Drawing.Point(202, 313);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Connect Database";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
